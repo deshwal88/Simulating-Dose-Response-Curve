2ba44be959a9868268bd659f010e35e538778380
deshwal88 <deshwalkanu88@gmail.com>
2025-10-17 15:05:33 -0400

    initial commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+"""
+functions.py
+
+Minimal module exposing a function for import.
+"""
+
+
+__all__ = ["compute_response_curve"]
+
+def compute_response_curve():
+    return True
+
+if __name__ == "__main__":
+    print("This module provides the compute_response_curve function for import.")
